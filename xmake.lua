be9aab09933fae12febacafc61061bbdd9d3fa6d
averdon <anthonyverdon10@gmail.com>
2025-04-01 13:36:11 +0200

    setup project

---
+++ xmake.lua
@@ -0,0 +1,18 @@
+set_languages("cxx17")
+
+set_toolchains("clang")
+set_warnings("allextra", "error")
+set_optimize("fastest")
+set_symbols("debug")
+
+add_requires("imgui", {configs = {glfw = true, opengl3 = true}})
+
+includes("submodules/Engine")
+
+target("42Run")
+    set_targetdir("./")
+    set_kind("binary")
+    add_files("srcs/**.cpp")
+    add_includedirs("srcs")
+    add_packages("imgui")
+    add_deps("Engine")
