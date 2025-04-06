f2130def8150995efc3a991303955449f9413a59
averdon <anthonyverdon10@gmail.com>
2025-04-06 14:56:46 +0200

    start implement movement
    only dash left/right with no limit when on borders

--- srcs/Player/Player.cpp
+++ srcs/Player/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player/Player.hpp"
 #include "Engine/3D/ModelManager/ModelManager.hpp"
 #include "Engine/Time/Time.hpp"
+#include "Engine/WindowManager/WindowManager.hpp"
 
 Player::Player()
 {
@@ -18,7 +19,29 @@ Player::~Player()
 void Player::ProcessInput()
 {
     direction = ml::normalize(ml::vec3(-sinf(angle), 0, cosf(angle)));
-    position += direction * Time::getDeltaTime() * speed;
+    
+    std::string currentAnim = ModelManager::GetModel(modelIndex).GetCurrentAnimation();
+    
+    if (currentAnim == "Run")
+    {
+        position += direction * speed * Time::getDeltaTime();
+        if (WindowManager::IsInputPressed(GLFW_KEY_D))
+            ModelManager::GetModel(modelIndex).Play("DashRight");
+        else if (WindowManager::IsInputPressed(GLFW_KEY_A))
+            ModelManager::GetModel(modelIndex).Play("DashLeft");
+    }
+    else if (currentAnim == "DashRight")
+    {
+        position += (direction * speed + ml::vec3(-1, 0, 0) * 2.0f) * Time::getDeltaTime();
+        if (ModelManager::GetModel(modelIndex).CurrentAnimationEnded())
+            ModelManager::GetModel(modelIndex).Play("Run");
+    }
+    else if (currentAnim == "DashLeft")
+    {
+        position += (direction * speed + ml::vec3(1, 0, 0) * 2.0f) * Time::getDeltaTime();
+        if (ModelManager::GetModel(modelIndex).CurrentAnimationEnded())
+            ModelManager::GetModel(modelIndex).Play("Run");
+    }
 }
 
 void Player::Draw(const ml::vec3 &camPos, const std::vector<std::unique_ptr<ALight>> &lights, const ml::mat4 &projection, const ml::mat4 &view)
