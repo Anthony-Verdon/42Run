71a3cefc0ed07cf2cd498f7b2c51e810eb22e77d
averdon <anthonyverdon10@gmail.com>
2025-04-09 14:51:27 +0200

    update way to handle PATH_TO_ENGINE define

--- xmake.lua
+++ xmake.lua
@@ -8,7 +8,8 @@ set_symbols("debug")
 add_requires("imgui", {configs = {glfw = true, opengl3 = true}})
 
 includes("submodules/Engine/")
-add_defines("PATH_TO_ENGINE=\"submodules/Engine/\"")
+
+set_config("PATH_TO_ENGINE", "submodules/Engine/")
 
 target("42Run")
     set_targetdir("./")
